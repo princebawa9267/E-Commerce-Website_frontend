ff9905165fb7ea0c6e992817bfd04445846ba349
princebawa9267 <136613646+princebawa9267@users.noreply.github.com>
2025-08-07 12:47:25 +0530

    More changes have done

--- src/Util/uploadToCloudnary.jsx
+++ src/Util/uploadToCloudnary.jsx
@@ -1,6 +1,13 @@
 export const uploadToCloudinary = async(pics) => {
-    const cloud_name = "dky5cdgsh"
-    const upload_preset = "prime-goods"
+    const cloud_name = import.meta.env.VITE_CLOUD_NAME;
+    const upload_preset = import.meta.env.VITE_UPLOAD_PRESET;
+
+    console.log("Cloudinary Config:", {
+        cloud_name,
+        upload_preset
+    });
+    // const cloud_name = "dky5cdgsh"
+    // const upload_preset = "prime-goods"
 
     if(pics){
         const data = new FormData();
@@ -8,10 +15,10 @@ export const uploadToCloudinary = async(pics) => {
         data.append("upload_preset",upload_preset);
         data.append("cloud_name",cloud_name);
 
-        const res = await fetch("https://api.cloudinary.com/v1_1/dky5cdgsh/image/upload",{
-            method : "POST",
-            body : data
-        })
+        const res = await fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, {
+                method: "POST",
+                body: data
+            });
 
         const fileData = await res.json();
         return fileData.url;
