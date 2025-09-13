6bcf57a1e1f8fa7a9139c0128d89814fb7d74ced
princebawa9267 <136613646+princebawa9267@users.noreply.github.com>
2025-09-13 09:55:18 +0530

    store update

--- src/state/store.jsx
+++ src/state/store.jsx
@@ -1,34 +1,35 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux";
-import { thunk } from "redux-thunk";
-import sellerSlice from "./seller/sellerSlice"
+
+import sellerSlice from "./seller/sellerSlice";
 import sellerProductSlice from "./seller/sellerProductSlice";
-import productSlice from "./customer/productSlice"
-import authSlice from "./authSlice"
-import cartSlice from "./customer/cartSlice"
+import productSlice from "./customer/productSlice";
+import authSlice from "./authSlice";
+import cartSlice from "./customer/cartSlice";
 import orderSlice from "./customer/orderSlice";
-import wishlistSlice from './customer/wishlistSlice'
+import wishlistSlice from "./customer/wishlistSlice";
 import sellerOrderSlice from "./seller/sellerOrderSlice";
 import transactionSlice from "./seller/transactionSlice";
 
 // 1. Combine your reducers here
 const rootReducer = combineReducers({
-  seller : sellerSlice,
-  sellerProduct : sellerProductSlice,
-  product : productSlice,
-  auth : authSlice,
-  cart : cartSlice,
-  order : orderSlice,
-  wishlist : wishlistSlice,
-  sellerOrder : sellerOrderSlice,
-  transactions : transactionSlice
+  seller: sellerSlice,
+  sellerProduct: sellerProductSlice,
+  product: productSlice,
+  auth: authSlice,
+  cart: cartSlice,
+  order: orderSlice,
+  wishlist: wishlistSlice,
+  sellerOrder: sellerOrderSlice,
+  transactions: transactionSlice,
 });
 
 // 2. Create the store
 const store = configureStore({
   reducer: rootReducer,
+  // no need to add thunk manually; it's already included
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(thunk),
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
 // 3. Custom hooks (for convenience)
